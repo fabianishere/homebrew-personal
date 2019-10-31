ccbefeea450c0dac81acb66dc677cd203ed96a0c
Fabian Mastenbroek <mail.fabianm@gmail.com>
2019-10-31 20:08:09 +0100

    iconsync 1.1

--- Formula/iconsync.rb
+++ Formula/iconsync.rb
@@ -1,8 +1,8 @@
 class Iconsync < Formula
   desc "A tool to sync your icon theme on macOS"
   homepage "https://github.com/fabianishere/iconsync"
-  url "https://github.com/fabianishere/iconsync/archive/v1.0.0.tar.gz"
-  sha256 "16f56620f579380699bad22923d3c55e4f757125d52722b02a74ad3e5bd295d1"
+  url "https://github.com/fabianishere/iconsync/archive/v1.1.0.tar.gz"
+  sha256 "638473810594e8832815698ed3b960ec5e337b41e2fae66932e32a97d19f95aa"
   head "https://github.com/fabianishere/iconsync.git"
 
 
